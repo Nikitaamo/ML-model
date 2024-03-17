e8b3c9da7b539fe768dafcac420f00680a834bcb
Nikitaamo <156019234+Nikitaamo@users.noreply.github.com>
2024-03-17 17:01:02 +0200

    Update src.py

--- src/src.py
+++ src/src.py
@@ -1,5 +1,4 @@
 import ssl
-import numpy as np
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
